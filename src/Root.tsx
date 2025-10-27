123e754c2865a8c0b5f5e9c8c04a028a806ae5ae
JulianCooke <juliancooke88@gmail.com>
2025-10-27 19:05:25 -0300

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="MyComp"
         component={MyComposition}
-        durationInFrames={310}
+        durationInFrames={250}
         fps={30}
         width={1080}
         height={1920}
